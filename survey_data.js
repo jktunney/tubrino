e56885646ec80340813221dbe721098034e41f42
Jake <jktunney5@gmail.com>
2016-04-23 16:11:18 -0400

    Adds correct question

--- survey_data.js
+++ survey_data.js
@@ -1,15 +1,7 @@
 // Hard coded survey questions
 module.exports = [
     {
-        text: 'Please tell us your age.',
-        type: 'number'
-    },
-    {
-        text: 'Have you ever jump-kicked a lemur?',
-        type: 'boolean'
-    },
-    {
-        text: 'Who is your favorite Teenage Mutant Ninja Turtle and why?',
+        text: 'Please tell us your address.',
         type: 'text'
     }
 ];
